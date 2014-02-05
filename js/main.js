c97c895db16555d0649ca41c05db2466062a9c9b
Keith Anderson <keithanderson@boldier.com>
2014-02-04 22:19:35 -0800

    Version 1.5
    -Added delete li when click trash button
    -hover on li to see delete button
    -still trying to work on adding multiple lis

--- js/main.js
+++ js/main.js
@@ -5,31 +5,36 @@ $(document).ready(function() {
 	 event.stopPropagation();
 
 		var value = $('.textarea').val();
-		$('ol.list li:first-child span').text(value);
-		if (event.keycode == 13) {
-			$(this).append(function(){
 
-			});
-		};
+
+///////////////ADD TEXT TO FIRST LI////////////////////////
+
+		$('.textarea').bind('keydown', function(event){
+			if(event.keyCode == 13){
+				$('ol.list li span').first().text(value);	
+				 return false;
+			}
+		});
 	});
+//////// I am not sure what to do next. I want to add a function that will take place after the keydown on the first li element is done.
+
+//		$(this).next(function(event){
+//			$('ol.list li span').append('li class="item"><span>').text(value).append('</span><img src="assets/trash1.png" class="trash" /></li');
+//		});
 
 
+////////////////HOVER LI TO SHOW TRASH/////////////////////
 
+		$('ol.list li.item').hover(function() {
+			$(this).find('.trash').css('display', 'block');
+		}, function() {
+			$(this).find('.trash').css('display', 'none');
+		});
 
+//////////////REMOVE LI WHEN CLICKING ON .TRASH//////////////
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		$('ol.list li.item').find('.trash').on('click', function(event) {
+			event.preventDefault();
+			$(this).closest('li').remove();
+		});
 });
